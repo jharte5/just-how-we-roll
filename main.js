7bbcc1f74b378d10b2f6a0b1fbf74b8d557b60a1
jharte5 <jonathan.harte@codeimmersives.com>
2019-11-19 16:41:16 -0500

    random die working

--- main.js
+++ main.js
@@ -34,11 +34,33 @@ function getRandomNumber(max) {
 /*******************
  * EVENT LISTENERS *
  *******************/
-const rotatePix = document.querySelector('#d6-roll')  
-function changePic (){
-    
+function rollOne () {
+    let roll =  getRandomNumber(6) 
+    document.querySelector('#d6-roll').src=`./images/d6/${roll}.png`
 }
+document.querySelector('#d6-roll').addEventListener('click', rollOne )
 
+function rollTwo () {
+    let roll = getRandomNumber(6)
+    let rollAgain = getRandomNumber(6)
+    document.querySelector('#double-d6-roll-1').src=`./images/d6/${roll}.png`
+    document.querySelector('#double-d6-roll-2').src=`./images/d6/${rollAgain}.png`
+}
+document.querySelector('#double-d6-roll-1').addEventListener('click', rollTwo )
+document.querySelector('#double-d6-roll-2').addEventListener('click', rollTwo )
+
+function rollTwelve () {
+    let roll = getRandomNumber(12)
+    document.querySelector('#d12-roll').src=`./images/numbers/${roll}.png`
+
+}
+document.querySelector('#d12-roll').addEventListener('click', rollTwelve)
+
+function rollTwenty () {
+    let roll = getRandomNumber(20)
+    document.querySelector('#d20-roll').src=`./images/numbers/${roll}.png`
+}
+document.querySelector('#d20-roll').addEventListener('click', rollTwenty)
 /****************
  * MATH SECTION *
  ****************/
